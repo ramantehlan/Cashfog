8cc2f5fa88694d8c1a4db56ac6eb6dd1e2ebc7fd
Raman Tehlan <ramantehlan@gmail.com>
2017-11-16 00:05:36 +0530

    Change in database information for heroku

--- includes/connect.inc.php
+++ includes/connect.inc.php
@@ -6,10 +6,10 @@ creator:-			Raman Tehlan
 Date of creation:-	13/11/2016
 ******************************************************************/
 
-$db_host       		='127.0.0.1';
-$db_user       		='root';
-$db_password   		='';
-$db_name       		='app';
+$db_host       		='ec2-23-21-101-174.compute-1.amazonaws.com';
+$db_user       		='zmbynibjuxwxdb';
+$db_password   		='064ee7d4bde65023435c34cfda7039e93e621c348a850b766d7ac2bc4406e8aa';
+$db_name       		='zmbynibjuxwxdb';
 
 /********************************
 table names are defined here
